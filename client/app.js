5040732e74e502b69e19e276fc4177b3563c9063
Trevor Finsaas <finsaasfamily@hotmail.com>
2015-12-02 10:22:10 -0700

    Fixed app.js to be lint compliant.

--- client/app.js
+++ client/app.js
@@ -1,13 +1,14 @@
+var React = require('react');
 var App = React.createClass({
-    render: function() {
-        return (
-          <div className="jumbotron">
-              <ul>
-                <h1 id="home-title">  TestJS </h1>
-              </ul>
-          </div>
-        );
-    }
+  render: function() {
+    return (
+      <div className="jumbotron">
+          <ul>
+            <h1 id="home-title">  TestJS </h1>
+          </ul>
+      </div>
+    );
+  }
 });
 
 React.render(<App/>, document.getElementById('first-container'));
