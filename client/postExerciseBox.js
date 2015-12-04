fac8187206b8901e7785cad3383c73741cb0025a
Trevor Finsaas <finsaasfamily@hotmail.com>
2015-12-04 10:15:53 -0700

    postExerciseBox.js is linted

--- client/postExerciseBox.js
+++ client/postExerciseBox.js
@@ -2,89 +2,91 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var ExerciseBox = React.createClass({
-  handleSubmit(e){
+  propTypes: {
+   url: React.PropTypes.string.isRequired
+ },
+  handleSubmit(e) {
 
-    e.preventDefault();
+   e.preventDefault();
 
-    var problem = ReactDOM.findDOMNode(this.refs.problem).value.trim();
-    var answer = ReactDOM.findDOMNode(this.refs.answer).value.trim();
+   var problem = ReactDOM.findDOMNode(this.refs.problem).value.trim();
+   var answer = ReactDOM.findDOMNode(this.refs.answer).value.trim();
 
-    if(!problem){
-      return;
-    }
+   if (!problem) {
+     return;
+   }
 
-    var data = ({problem: problem, answer: answer})
+   var data = ({problem: problem, answer: answer});
 
-  $.ajax({
+   $.ajax({
      url: this.props.url,
      dataType: 'json',
      data: data,
-     type:'POST',
-     success: function(response){
-       console.log("posting data!",data, response)
-       document.location='/post_exercise'
-     }.bind(this),
-     error: function(xhr, status, err){
-       console.log("not posting data!")
+     type: 'POST',
+     success: function(response) {
+       console.log('posting data!', data, response);
+       document.location = '/post_exercise';
+     },
+     error: function(xhr, status, err) {
+       console.log('not posting data!');
        console.error(this.props.url, status, err.toString());
      }.bind(this)
-   })
+   });
  },
- handleUpdate(id){
+  handleUpdate(id) {
 
-    var id = id;
-    var problem = React.findDOMNode(this.refs.problem).value.trim();
-    var answer = React.findDOMNode(this.refs.answer).value.trim();
+   var problem = React.findDOMNode(this.refs.problem).value.trim();
+   var answer = React.findDOMNode(this.refs.answer).value.trim();
 
-    console.log(id);
-    if(!problem){
-      return;
-    }
+   console.log(id);
+   if (!problem) {
+     return;
+   }
 
-    var data = ({problem: problem, answer: answer});
+   var data = ({problem: problem, answer: answer});
 
-    $.ajax({
-      url: this.props.url + id,
-      dataType: 'json',
-      data: data,
-      type:'PUT',
-      success: function(response){
-        console.log("Updating Exercise!", data, response)
-        document.location='/post_exercise'
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log("Did not <update></update>!")
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    })
-  },
+   $.ajax({
+     url: this.props.url + id,
+     dataType: 'json',
+     data: data,
+     type: 'PUT',
+     success: function(response) {
+       console.log('Updating Exercise!', data, response);
+       document.location = '/post_exercise';
+     },
+     error: function(xhr, status, err) {
+       console.log('Did not <update></update>!');
+       console.error(this.props.url, status, err.toString());
+     }.bind(this)
+   });
+ },
 
- render: function() {
+  render: function() {
    return (
 
-    <div>
-      <div className="col-sm-6 col-md-12">
-        <h1>Enter New Exercises</h1>
-      </div>
+   <div>
+     <div className="col-sm-6 col-md-12">
+       <h1>Enter New Exercises</h1>
+     </div>
 
-        <form>
-          <div className="col-sm-6 col-md-6">
-            <div className="form-group">
-              <label>Exercise Problem</label>
-              <textarea rows="15" type="text" className="form-control" ref="problem" placeholder="Exercise Problem" />
-            </div>
-            <div className="form-group">
-              <label>Exercise Answer</label>
-              <textarea rows="15" type="text" ref="answer" className="form-control" placeholder="Exercise Answer" />
-            </div>
-            <div className="form-group">
-              <button onClick={this.handleSubmit} type="submit" className="btn btn-default"> Submit </button>
-            </div>
-          </div>
-        </form>
-    </div>
-    )
-  }
+       <form>
+         <div className="col-sm-6 col-md-6">
+           <div className="form-group">
+             <label>Exercise Problem</label>
+             <textarea rows="15" type="text" className="form-control" ref="problem" placeholder="Exercise Problem" />
+           </div>
+           <div className="form-group">
+             <label>Exercise Answer</label>
+             <textarea rows="15" type="text" ref="answer" className="form-control" placeholder="Exercise Answer" />
+           </div>
+           <div className="form-group">
+             <button onClick={this.handleSubmit} type="submit" className="btn btn-default"> Submit </button>
+           </div>
+         </div>
+       </form>
+   </div>
+   );
+ }
 });
 
-ReactDOM.render(<ExerciseBox url='/api/exercises/'/>, document.getElementById('post-exercise'));
+ReactDOM.render(<ExerciseBox url = "/api/exercises/"/>, document.getElementById('post-exercise'));
