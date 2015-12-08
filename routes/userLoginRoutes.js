137bd4eb7e298019fa63dacc5720c10b4c888ebc
Trevor Finsaas <finsaasfamily@hotmail.com>
2015-12-08 14:04:14 -0700

    modals working for the login and signup

--- routes/userLoginRoutes.js
+++ routes/userLoginRoutes.js
@@ -1,18 +1,12 @@
+var mongoose = require('mongoose');
+var User = require('../models/user');
+
 module.exports = function(app, passport) {
-  // route middleware to make sure a user is logged in
-  function isLoggedIn(req, res, next) {
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-  }
-    // =====================================
-    // HOME PAGE (with login links) ========
-    // =====================================
+
   app.get('/', function(req, res) {
-    res.render('index.ejs'); // load the index.ejs file
+    res.render('index.ejs', {
+      user  : req.user
+    });
   });
 
     // =====================================
@@ -27,7 +21,7 @@ module.exports = function(app, passport) {
 
     // process the login form
   app.post('/login', passport.authenticate('local-login', {
-    successRedirect: '/', // redirect to the secure profile section
+    successRedirect: '/profile', // redirect to the secure profile section
     failureRedirect: '/login', // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
   }));
@@ -48,30 +42,13 @@ module.exports = function(app, passport) {
     failureRedirect: '/signup', // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
   }));
-    // app.post('/signup', do all our passport stuff here);
 
-    // =====================================
-    // PROFILE SECTION =====================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
+
   app.get('/profile', isLoggedIn, function(req, res) {
     res.render('profile.ejs', {
-      user: req.user // get the user out of session and pass to template
+      user: req.user 
     });
   });
-
-  // app.get('/profile', isLoggedIn, function(req, res) {
-  //       mongoose.model('Answer').find({
-  //           user: req.user._id
-  //       }).populate('exercise').exec(function(err, comments) {
-  //           res.render('profile.ejs', {
-  //               user: req.user, // get the user out of session and pass to template
-  //               answers: answers
-  //           });
-  //       })
-  //   });
-
 
   app.get('/post_exercise', isLoggedIn, function(req, res) {
     res.render('post_exercise.ejs', {
@@ -79,11 +56,40 @@ module.exports = function(app, passport) {
     });
   });
 
-    // =====================================
-    // LOGOUT ==============================
-    // =====================================
+    
   app.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
   });
+
+
+  app.get('/admin', isAdmin, function(req, res) {
+    mongoose.model('User').find({}, function(err, users){
+      if(err){
+        return console.log(err);
+      } else {
+        res.render('adminProfile.ejs', {
+          users : users,
+          user : req.user
+        });
+      }
+    });
+  });
+
 };
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+      return next();
+  }
+    res.redirect('/');
+}
+
+
+function isAdmin(req, res, next) {
+  if(req.isAuthenticated() && req.user.local.role === 'admin')
+
+    return next();
+
+  res.redirect('/');
+}
