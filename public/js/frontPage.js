91f296efdf59b826451aacf9cfd33f663eb74fde
ed42311 <ed42311@gmail.com>
2015-12-10 10:37:50 -0700

    the animating is working need to integrate, also file structure added, new frontpage, new routes

---
+++ public/js/frontPage.js
@@ -0,0 +1,97 @@
+function drawAllToCanvas () {
+        window.requestAnimFrame = (function(callback) {
+          return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+            function(callback) {
+              window.setTimeout(callback, 1000 / 60);
+            };
+        })();
+
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        var r = 200;
+
+        var start = {
+          x: 100,
+          y: 300
+        };
+
+        var myLine = {
+          x: c.width / 4,
+          y: c.height / 2,
+          width: 10,
+          height: 2,
+          borderWidth: 2
+        }
+        
+
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = 'red';
+        ctx.shadowOffsetX = 0;
+        ctx.shadowOffsetY = 0;
+        ctx.shadowBlur = 10;
+        ctx.shadowColor = 'gray';
+
+        function drawLineAcross () {
+
+          function drawLine (myLine, ctx) {
+           
+            ctx.beginPath();
+            ctx.moveTo((start.x + r), start.y);
+            ctx.rect(myLine.x, myLine.y, myLine.width, myLine.height);
+            ctx.lineWidth = myLine.borderWidth;
+            ctx.stroke();
+
+          }
+
+          function animate(myline, c, ctx, startTime) {
+
+            var time = (new Date()).getTime() - startTime;
+            var linearSpeed = 300;
+            var newX = linearSpeed * time / 1000;
+              if(newX < c.width - myLine.width - myLine.borderWidth / 2) {
+                myLine.x = newX;
+              }
+
+            drawLine(myLine, ctx);
+
+            requestAnimFrame(function() {
+              animate(myLine, c, ctx, startTime);
+            });
+            }
+
+            drawLine(myLine, ctx);
+
+            // wait one second before starting animation
+            setTimeout(function() {
+            var startTime = (new Date()).getTime();
+            animate(myLine, c, ctx, startTime);
+            }, 1000);
+        }
+
+        function drawCircle () {
+          var end = 190;
+          var cur = 0;
+          var counterClockwise = false;
+          var circ = Math.PI * 2;
+          var quart = Math.PI / 220;
+
+          function animate(current) {
+            ctx.beginPath();
+            ctx.arc(start.x, start.y, r, -(quart), ((circ) * current) - quart, false);
+            ctx.stroke();
+              cur++;
+              if (cur < end) {
+                requestAnimFrame(function () {
+                animate(cur / 180)
+                });
+              } else {
+                drawLineAcross();
+              }
+          }
+          animate();
+        }
+        drawCircle();
+      };
+      drawAllToCanvas();
+
+// module.exports =  drawAllToCanvas();
